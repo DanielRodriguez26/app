fb53ca8f9e71ea0886b031bd93d2616050474153
Daniel Rodriguez <darc2611@gmail.com>
2020-08-22 01:21:48 -0500

    Third commit

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from core.erp.models import Category, Product
 
 
-# Create your views here.
+
 def myfirstview(request):
-  return HttpResponse('hello')
+  data = {
+    'name' : 'Daniel',
+    'categories' : Category.objects.all()
+
+  }
+  return render(request, 'index.html', data)
+
+def mysecondview(request):
+  data = {
+    'name' : 'Daniel',
+    'products' : Product.objects.all()
+
+  }
+  return render(request, 'second.html', data)
