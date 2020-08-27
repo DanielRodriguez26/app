32f59a4c796509af745f47ca0d78856b4b4a9416
Daniel Rodriguez <darc2611@gmail.com>
2020-08-27 16:38:43 -0500

    Fifth commit

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 
     path('category/list/', CategoryListView.as_view(), name='category_list'),
     path('category/add/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/edit/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
+
 
 ]
--- core/erp/views/category/view.py
+++ core/erp/views/category/view.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from core.erp.forms import CategoryForm
 from core.erp.models import Category
@@ -30,10 +30,16 @@ class CategoryListView(ListView):
   def post(self, request, *args, **kwargs):
     data = {}
     try:
-      data = Category.objects.get(pk=request.POST['id']).toJson()
+      action = request.POST['action']
+      if action == 'searchdata':
+        data = []
+        for i in Category.objects.all():
+          data.append(i.toJson())
+      else:
+        data['error'] = 'An error has occurred'
     except Exception as e:
       data['error'] = str(e)
-    return JsonResponse(data)
+    return JsonResponse(data, safe=False)
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
@@ -55,10 +61,7 @@ class CategoryCreateView(CreateView):
       action = request.POST['action']
       if action == 'add':
         form = self.get_form()
-        if form.is_valid():
-          form.save()
-        else:
-          data['error'] = form.errors
+        data = form.save()
       else:
         data['error'] = 'You have not entered any option'
     except Exception as e:
@@ -73,4 +76,58 @@ class CategoryCreateView(CreateView):
     context['list_url'] = reverse_lazy('erp:category_list')
     return context
 
+class CategoryUpdateView(UpdateView):
+  model = Category
+  form_class = CategoryForm
+  template_name = 'category/create.html'
+  success_url = reverse_lazy('erp:category_list')
 
+  def dispatch(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    return super().dispatch(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+    data = {}
+    try:
+      action = request.POST['action']
+      if action == 'add':
+        form = self.get_form()
+        data = form.save()
+      else:
+        data['error'] = 'You have not entered any option'
+    except Exception as e:
+      data['error'] = str(e)
+    return JsonResponse(data)
+
+  def  get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['title'] = 'Category Edit'
+    context['entity'] = 'Category'
+    context['action'] = 'edit'
+    context['list_url'] = reverse_lazy('erp:category_list')
+    return context
+
+class CategoryDeleteView(DeleteView):
+  model = Category
+  form_class = CategoryForm
+  template_name = 'category/delete.html'
+  success_url = reverse_lazy('erp:category_list')
+
+  def dispatch(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    return super().dispatch(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+    data = {}
+    try:
+      self.object.delete()
+    except Exception as e:
+      data['error'] = str(e)
+    return JsonResponse(data)
+
+  def  get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['title'] = 'Category Delete'
+    context['entity'] = 'Category'
+    context['list_url'] = reverse_lazy('erp:category_list')
+    return context
